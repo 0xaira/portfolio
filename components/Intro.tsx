9f4e91c8ac1e7f065331b882bce570a3b43e5c98
Aira Jena <airajena.hack@gmail.com>
2024-04-09 23:39:23 +0530

    feat: Added the profile description in the Intro

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,11 +1,14 @@
-'use client'
-import { motion } from 'framer-motion'
-import React from 'react'
+"use client";
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
 import profile from "@/public/profile.jpeg";
-import Image from 'next/image';
 export default function Intro() {
   return (
-    <section>
+    <section
+      id="home"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+    >
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
@@ -16,9 +19,9 @@ export default function Intro() {
               duration: 0.2,
             }}
           >
-            <Image
-              src={profile}
-              alt="Aira portrait"
+          <Image
+          src={profile}
+          alt="Aira portrait"
               width="192"
               height="192"
               quality="95"
@@ -42,6 +45,18 @@ export default function Intro() {
           </motion.span>
         </div>
       </div>
+
+    <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+    >
+        <span className="font-bold">Hello, I&apos;m Aira.</span> I&apos;m a{" "}
+        <span className="font-bold">full-stack developer</span> with{" "}
+        <span className="font-bold">2 years</span> of experience. I enjoy
+        building <span className="italic">sites & webapps</span>. My focus is{" "} <br></br>
+        <span className="underline">Next.js & Node.js</span>.
+    </motion.h1>
     </section>
-  )
+  );
 }
