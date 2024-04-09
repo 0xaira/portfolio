4380db792ec6ad9c3aac971241368b31adddec9d
Aira Jena <airajena.hack@gmail.com>
2024-04-09 23:15:04 +0530

    style: Set the Intro profile image

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Header from "@/components/Header";
-
+import Intro from "@/components/Intro";
 export default function Home() {
   return (
-    <main className="">   
-        <Header />  
+    <main className="flex flex-col items-center px-4">   
+         <Intro />
     </main>
   );
 }
