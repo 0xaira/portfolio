255727eab94f03767e8c5dcabd8ce7c5442c4e4e
Aira Jena <airajena.hack@gmail.com>
2024-04-10 12:44:14 +0530

    feat: Built the Projects Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import About from "@/components/About";
 import Intro from "@/components/Intro";
 import SectionDivider from "@/components/SectionDivider";
+import Projects from "@/components/projects";
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">   
          <Intro />
          <SectionDivider />
          <About />
+         <Projects />
     </main>
   );
 }
