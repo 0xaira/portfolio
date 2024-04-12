a5815ee288fe4753f746b95dbe6ff03b0e30004c
Aira Jena <airajena.hack@gmail.com>
2024-04-12 17:47:34 +0530

    Feat: Added view code,details and live button to the projects

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,17 +1,22 @@
-"use client";
+// Import necessary modules
 import { projectsData } from "@/lib/data";
 import Image from "next/image";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
+import { RiPagesLine } from "react-icons/ri";
+import { FaCode, FaRegFileCode } from "react-icons/fa";
 
+// Define the type for project props
 type ProjectProps = (typeof projectsData)[number];
 
+// Project component
 export default function Project({
   title,
   description,
   tags,
   imageUrl,
 }: ProjectProps) {
+  // Ref for scroll animation
   const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
     target: ref,
@@ -20,6 +25,7 @@ export default function Project({
   const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
   const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
 
+  // JSX
   return (
     <motion.div
       ref={ref}
@@ -29,13 +35,18 @@ export default function Project({
       }}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+      {/* Main container */}
+      <section className="bg-gray-100 max-w-[50rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] lg:h-[22rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20" style={{ marginBottom: "2rem" }}>
+        {/* Content container */}
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
+          {/* Title */}
           <h3 className="text-2xl font-semibold">{title}</h3>
+          {/* Description */}
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
             {description}
           </p>
-          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+          {/* Tags */}
+          <ul className="flex flex-wrap mt-4 gap-2">
             {tags.map((tag, index) => (
               <li
                 className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
@@ -45,24 +56,42 @@ export default function Project({
               </li>
             ))}
           </ul>
-        </div>
+          {/* Buttons */}
+          <div className="flex mt-4 gap-4">
+          <button className="flex items-center gap-2 px-2 py-1 bg-gray-900 text-white rounded-full transition hover:bg-gray-800 focus:outline-none">
+              <span>Details</span>
+              <RiPagesLine />
+            </button>
 
+
+            <button className="flex items-center gap-2 px-2 py-1 bg-gray-900 text-white rounded-full transition hover:bg-gray-800 focus:outline-none">
+              <span> Code</span>
+              <FaCode />
+            </button>
+            {/* Live button */}
+            <button className="flex items-center gap-2 px-2 py-1 bg-gray-900 text-white rounded-full transition hover:bg-gray-800 focus:outline-none">
+              <span>Live</span>
+              <FaRegFileCode />
+            </button>
+          </div>
+        </div>
+        {/* Image */}
         <Image
           src={imageUrl}
           alt="Project I worked on"
           quality={95}
-          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
-        transition 
-        group-hover:scale-[1.04]
-        group-hover:-translate-x-3
-        group-hover:translate-y-3
-        group-hover:-rotate-2
+          className="absolute hidden sm:block top-8 -right-10 w-[28.25rem] rounded-t-lg shadow-2xl
+          transition 
+          group-hover:scale-[1.04]
+          group-hover:-translate-x-3
+          group-hover:translate-y-3
+          group-hover:-rotate-2
 
-        group-even:group-hover:translate-x-3
-        group-even:group-hover:translate-y-3
-        group-even:group-hover:rotate-2
+          group-even:group-hover:translate-x-3
+          group-even:group-hover:translate-y-3
+          group-even:group-hover:rotate-2
 
-        group-even:right-[initial] group-even:-left-40"
+          group-even:right-[initial] group-even:-left-40"
         />
       </section>
     </motion.div>
